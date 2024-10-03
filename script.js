ea492c801fe7e92b265e51bcefe4750dc49a9506
Emmay17 <emmamayele123@gmail.com>
2024-10-03 16:50:45 +0100

    modification function done

---
+++ script.js
@@ -0,0 +1,27 @@
+const modification = document.querySelectorAll(".modification");
+
+modification.forEach( function(button){
+    button.addEventListener('Click', function(){
+        let taskId = button.getAttribute("data-id");
+        modification_tasks(taskId)
+    })
+})
+
+function modification_tasks(id){
+    let recherche = taches.find( (tache) => tache.id === id);
+    recherche.paragraphe = String(prompt("votre modification"));
+}
+
+
+
+let taches = [{
+    auteur: "emmanuel",
+    id: 0,
+    paragraphe: "faire les taches menager",
+    etat : false
+},{
+    auteur: "Gad",
+    id: 1,
+    paragraphe: "bonjour tout le monde",
+    etat : false
+}]
