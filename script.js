620214f1e48fbc194a7f8bf708fc8c33677e4ec4
rohanito006 <nyogbarohan@gmail.com>
2024-10-03 21:03:45 +0100

    input

---
+++ script.js
@@ -0,0 +1,25 @@
+const div = document.querySelector("#container");
+
+ div.style.display = 'flex';
+ div.style.width = '20.5rem' ;
+ div.style.border = '0.1rem black';
+ div.style.gap = '2%'
+ div.style.margin = ' 10rem 0 0 10rem' ;
+ div.style.padding = '1rem 0 1rem 0'
+ div.style.background= 'red'
+
+
+ const input = document.querySelector("#nom")
+
+ input.style.width = '10rem';
+ 
+
+
+
+
+
+
+
+
+
+
